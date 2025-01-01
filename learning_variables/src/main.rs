686ae37d8484d7982baba3848c3edcf368511b88
Saber <92962589+smit4k@users.noreply.github.com>
2025-01-01 17:53:10 -0500

    add tuples to learning_variables

--- learning_variables/src/main.rs
+++ learning_variables/src/main.rs
@@ -42,4 +42,17 @@ fn main() {
     println!("{c}");
     println!("{z}");
     println!("{smiley_emoji}");
+
+    
+    println!("-------------------------------------");
+
+    // compound types
+
+    // allows us to group together multiple values of different types into one single type
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x,y,z) = tup;
+
+    println!("The value of y is: {y}");
 }
