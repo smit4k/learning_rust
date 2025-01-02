fe6d146dd72121a81495507338c824ff2d801bed
Saber <92962589+smit4k@users.noreply.github.com>
2025-01-02 14:36:40 -0500

    Add expressions and explanations for expressions

--- learning_functions/src/main.rs
+++ learning_functions/src/main.rs
@@ -2,6 +2,20 @@ fn main() {
     println!("Hello, world!");
     another_function(5);
     print_labeled_measurement(7, 'm');
+
+    // Expressions
+    // expressions do not have semicolons
+
+    let y = {
+        let x = 3;
+        x+1
+    };
+
+    println!("The value of y is: {y}");
+
+
+    let x = five();
+    println!("The value of x is: {x}");
 }
 
 fn another_function(x:i32) {
@@ -10,4 +24,10 @@ fn another_function(x:i32) {
 
 fn print_labeled_measurement(value: i32, label: char){
     println!("The measurement is {value}{label}");
+}
+
+
+// Even though this func only contains the number 5, it is still perfectly legal. (even its return type is i32)
+fn five() -> i32 {
+    5 // 5 is an expression because it doesn't have a semicolon and this is the expression we want to return.
 }
