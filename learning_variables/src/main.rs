c6ceb8200c3ecf72d7b9e309a161b34da791aaaf
Saber <92962589+smit4k@users.noreply.github.com>
2025-01-01 23:00:38 -0500

    Add comment explaining deconstruction of a tuple

--- learning_variables/src/main.rs
+++ learning_variables/src/main.rs
@@ -62,7 +62,7 @@ fn main() {
 
     let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    let (x,y,z) = tup;
+    let (x,y,z) = tup; // deconstructing tup and breaking it down into three seperate variables (x,y, and z)
 
     println!("The value of y is: {y}");
     
