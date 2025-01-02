4e2a03500d6931cd30f65f5609756ef29b404096
Saber <92962589+smit4k@users.noreply.github.com>
2025-01-02 14:10:19 -0500

    Add labeled measurement function

--- learning_functions/src/main.rs
+++ learning_functions/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     println!("Hello, world!");
     another_function(5);
+    print_labeled_measurement(7, 'm');
 }
 
 fn another_function(x:i32) {
     println!("The value of x is {x}");
+}
+
+fn print_labeled_measurement(value: i32, label: char){
+    println!("The measurement is {value}{label}");
 }
